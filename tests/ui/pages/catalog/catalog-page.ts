286eec082516d74d4540ada1c65f2a279c0b051d
Ilgam Shakirov <ishakirov@provectus.com>
2022-11-09 13:12:49 +0300

    Demo: define plain locators

--- tests/ui/pages/catalog/catalog-page.ts
+++ tests/ui/pages/catalog/catalog-page.ts
@@ -2,71 +2,68 @@ import Button from '../../elements/button';
 import InputField from '../../elements/input-field';
 import BasePage from '../base-page';
 
-const SELECTORS = {
-  filterWithSelect: filterName => `#select-label-id:has-text('${filterName}') >> ..`,
-  filterWithInput: filterName => `label:text-is("${filterName}") >> ..`,
-  searchBar: `[placeholder="Search"]`,
-  filterList: `[role="listbox"]`,
-  filterOption: `[role="option"]`,
-  filterWithInputOption: `[role="presentation"]`,
-  listItemName: name => `a:has-text('${name}')`,
-  tab: name => `[role="tab"]:has-text('${name}')`,
-  searchButton: `[placeholder="Search"] >> ..`,
-  noMatchesFound: `text=No matches found`,
-};
+const filterWithSelectLocator = filterName => `#select-label-id:has-text('${filterName}') >> ..`;
+const filterWithInputLocator = filterName => `label:text-is("${filterName}") >> ..`;
+const searchBarLocator = `[placeholder="Search"]`;
+const filterListLocator = `[role="listbox"]`;
+const filterOptionLocator = `[role="option"]`;
+const filterWithInputOptionLocator = `[role="presentation"]`;
+const listItemNameLocator = name => `a:has-text('${name}')`;
+const tabLocator = name => `[role="tab"]:has-text('${name}')`;
+const searchButtonLocator = `[placeholder="Search"] >> ..`;
+const noMatchesFoundLocator = `text=No matches found`;
+
 export default class CatalogPage extends BasePage {
   async openFilterWithSelect(filterName: string) {
-    await this.page.locator(SELECTORS.filterWithSelect(filterName)).click();
+    await this.page.locator(filterWithSelectLocator(filterName)).click();
   }
 
   async chooseOption(option: string) {
-    await this.page.click(
-      `${SELECTORS.filterList} >> ${SELECTORS.filterOption}:has-text('${option}')`,
-    );
+    await this.page.click(`${filterListLocator} >> ${filterOptionLocator}:has-text('${option}')`);
   }
 
   async searchByTextInFilter(filter: string, text: string) {
-    await this.page.locator(`${SELECTORS.filterWithInput(filter)} >> input`).fill(text);
-    await this.page.locator(`${SELECTORS.filterWithInputOption}:has-text('${text}')`).click();
+    await this.page.locator(`${filterWithInputLocator(filter)} >> input`).fill(text);
+    await this.page.locator(`${filterWithInputOptionLocator}:has-text('${text}')`).click();
   }
 
   async openFilterWithInput(filterName: string) {
-    await this.page.locator(SELECTORS.filterWithInput(filterName)).click();
+    await this.page.locator(filterWithInputLocator(filterName)).click();
   }
 
   async isListItemVisible(name: string): Promise<boolean> {
-    await this.page.locator(SELECTORS.listItemName(name)).waitFor({ state: 'visible' });
-    return this.page.locator(SELECTORS.listItemName(name)).isVisible();
+    await this.page.locator(listItemNameLocator(name)).waitFor({ state: 'visible' });
+    return this.page.locator(listItemNameLocator(name)).isVisible();
   }
 
   async clickOnListItem(name: string) {
-    await this.page.locator(SELECTORS.listItemName(name)).click();
+    await this.page.locator(listItemNameLocator(name)).click();
   }
 
   async clickTab(name: string) {
-    return this.page.locator(SELECTORS.tab(name)).click();
+    return this.page.locator(tabLocator(name)).click();
   }
 
   get searchBar() {
-    return new InputField(this.page, SELECTORS.searchBar);
+    return new InputField(this.page, searchBarLocator);
   }
 
   async fillSearchBar(text: string) {
     await this.searchBar.fill(text);
-    await this.page.locator(SELECTORS.searchBar).press('Enter');
+    await this.page.locator(searchBarLocator).press('Enter');
   }
 
   async isListEmpty(): Promise<boolean> {
-    await this.page.locator(SELECTORS.noMatchesFound).waitFor({ state: 'visible' });
-    return this.page.locator(SELECTORS.noMatchesFound).isVisible();
+    await this.page.locator(noMatchesFoundLocator).waitFor({ state: 'visible' });
+    return this.page.locator(noMatchesFoundLocator).isVisible();
   }
 
   async isListFull(): Promise<boolean> {
-    await this.page.locator(SELECTORS.noMatchesFound).waitFor({ state: 'hidden' });
-    return this.page.locator(SELECTORS.noMatchesFound).isHidden();
+    await this.page.locator(noMatchesFoundLocator).waitFor({ state: 'hidden' });
+    return this.page.locator(noMatchesFoundLocator).isHidden();
   }
 
   get searchButton() {
-    return new Button(this.page, SELECTORS.searchButton);
+    return new Button(this.page, searchButtonLocator);
   }
 }
